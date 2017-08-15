4162864877b823933e31969b919a638a9c7d2598
FAMILIA DUITAMA <FAMILIA DUITAMA@FAMILIADUITAMA>
2017-08-15 00:29:57 -0500

    creacion metodos

--- Metodo/src/git/Seanbox.java
+++ Metodo/src/git/Seanbox.java
@@ -6,5 +6,13 @@ package git;
  * @author FAMILIA DUITAMA
  */
 public class Seanbox {
+    public void Desicion (){
+        
+    }
+    public void Ciclo() {
+        
+    }
+    public void Operacion {
     
+    }
 }
