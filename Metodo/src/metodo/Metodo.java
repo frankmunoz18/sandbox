a6759b328ac19f3188ac0b33cf31a91c5138ce8f
FAMILIA DUITAMA <FAMILIA DUITAMA@FAMILIADUITAMA>
2017-08-15 00:05:02 -0500

    creacion clase

---
+++ Metodo/src/metodo/Metodo.java
@@ -0,0 +1,17 @@
+
+package metodo;
+
+/**
+ *
+ * @author FAMILIA DUITAMA
+ */
+public class Metodo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
